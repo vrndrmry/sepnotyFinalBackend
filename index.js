efbfc6922dc686c2722c6d8006975fbb697d2b68
Geetam <geetambinani6@gmail.com>
2024-04-20 10:48:41 +0530

    add article reference in user model

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ dotenv.config();
 
 // Middleware
 
-app.use(cors({ credentials: true, origin: "*" }));
+app.use(cors({ credentials: true, origin: true }));
+// app.use(cors({ credentials: true, origin: "*" }));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -27,7 +28,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
@@ -51,13 +52,13 @@ const connect = async () => {
 };
 
 // HTTPS Options
-const httpsOptions = {
-  key: fs.readFileSync('/etc/letsencrypt/live/backend.sepnoty.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/backend.sepnoty.com/fullchain.pem')
-};
+// const httpsOptions = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/backend.sepnoty.com/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/backend.sepnoty.com/fullchain.pem')
+// };
 
 // Create HTTPS Server
-const server = https.createServer(httpsOptions, app);
+// const server = https.createServer(httpsOptions, app);
 
 
 app.listen(process.env.PORT, async () => {
--- model/admin/userModel.js
+++ model/admin/userModel.js
@@ -1,30 +1,37 @@
 import { mongoose } from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        description:"User email ID",
-        required:true
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      description: "User email ID",
+      required: true,
     },
-    password:{
-        type:String,
-        description:"Password of user ",
-        required:true
+    password: {
+      type: String,
+      description: "Password of user ",
+      required: true,
     },
     blogs: [
-        {
-            type:mongoose.Types.ObjectId,
-            ref:"BlogModel",
-        }
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "BlogModel",
+      },
     ],
-      
-    
-    admin:{
-        type:Boolean,
-        description:"Admin access",
-        default:false
-    }
+    articles: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "ArticleModel",
+      },
+    ],
 
-},{timestamps:true})
+    admin: {
+      type: Boolean,
+      description: "Admin access",
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-export const UserModel = mongoose.model("UserModel",UserSchema)
+export const UserModel = mongoose.model("UserModel", UserSchema);
